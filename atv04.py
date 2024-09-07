1f78d1540e9115e27b6ce1934ee63efc44f52fa9
emanuel378 <emanuelmvieira10@gmail.com>
2024-09-07 18:08:30 -0300

    Media

---
+++ atv04.py
@@ -0,0 +1,6 @@
+# Crie um programa que receba três notas de um aluno e calcule a média aritmética.
+nota1 = float(input("Digite a sua primeira nota:"))
+nota2 = float(input("Digite a sua segunda nota:"))
+nota3 = float(input("Digite a sua terceira nota:"))
+media = (nota1 + nota2 + nota3) /3
+print(f"Sua media e {media}")
